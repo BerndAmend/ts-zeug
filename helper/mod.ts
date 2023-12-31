c96f3bbd2e6c5a126b06f692ea6f8cfc1fc1af80
Bernd Amend <bernd.amend@gmail.com>
2023-12-31 02:53:03 +0100

    copy abortable/deadline/delay from deno
    at least until I find a easy way to also use them in web browsers

--- helper/mod.ts
+++ helper/mod.ts
@@ -20,6 +20,10 @@ COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
 IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
+export * from "./abortable.ts";
+export * from "./deadline.ts";
+export * from "./delay.ts";
+
 export type Buffer =
   | ArrayLike<number>
   | Uint8Array
