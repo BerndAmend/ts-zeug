00c66adf3878390eee2fc37eb3987bbf9b5d5781
Bernd Amend <typescript@berndamend.de>
2024-01-09 21:29:35 +0100

    fix build_npm.ts npm package generation
    the package works with svelte, but not yset fully with node

--- build_npm.ts
+++ build_npm.ts
@@ -1,26 +1,34 @@
 // Copyright 2023-2024 Bernd Amend. MIT license.
-import { build, emptyDir } from "https://deno.land/x/dnt@0.38.1/mod.ts";
+import { build, emptyDir } from "https://deno.land/x/dnt@0.39.0/mod.ts";
 
 await emptyDir("./npm");
 
 await build({
   entryPoints: ["./mod.ts"],
   outDir: "./npm",
-  shims: {
-    deno: true,
-    webSocket: true,
+  compilerOptions: {
+    target: "Latest",
+    lib: ["ESNext", "DOM"],
   },
+  shims: {
+    deno: "dev",
+  },
+  typeCheck: false,
+  test: false,
+  declaration: "inline",
+  esModule: true,
+  scriptModule: false,
   package: {
     name: "ts-zeug",
     version: Deno.args[0],
-    description: "library for mqtt-ts, msgpack",
+    description: "Various stuff/zeug for typescript for deno, node and the web",
     license: "MIT",
     repository: {
       type: "git",
-      url: "",
+      url: "https://github.com/BerndAmend/ts-zeug.git",
     },
     bugs: {
-      url: "",
+      url: "https://github.com/BerndAmend/ts-zeug/issues",
     },
   },
   postBuild() {
