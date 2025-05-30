17a7008458e8bb8752ef070042aa05f5bf983abe
Bernd Amend <typescript@berndamend.de>
2025-05-31 01:46:59 +0200

    chore: update copyright

--- mod.ts
+++ mod.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright 2023-2024 Bernd Amend. MIT license.
+ * Copyright 2023-2025 Bernd Amend. MIT license.
  */
 export * as helper from "./helper/mod.ts";
 export * as msgpack from "./msgpack/mod.ts";
--- mqtt/mod.ts
+++ mqtt/mod.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright 2023-2024 Bernd Amend. MIT license.
+ * Copyright 2023-2025 Bernd Amend. MIT license.
  */
 import {
   type Branded,
